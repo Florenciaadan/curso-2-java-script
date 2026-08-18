ba836c91398c21fabdb89933b774bd936c366bd8
Florencia Adán <florenciaadan@hotmail.com>
2026-08-18 19:13:21 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const nombre = prompt("¿Cuál es tu nombre?");
+const destino = prompt("¿Cuál es tu destino?");
+const dias = parseInt(prompt("¿Cuántos días vas a viajar?"));
+const presupuestoDiario = parseFloat(
+    prompt("¿Cuánto dinero pensás gastar por día?")
+);
+
+const presupuestoTotal = dias * presupuestoDiario;
+
+const mensaje = `Hola ${nombre}. Tu viaje a ${destino} será de ${dias} días. 
+Si gastás aproximadamente $${presupuestoDiario} por día, 
+necesitarás un presupuesto estimado de $${presupuestoTotal}.`;
+
+console.log(mensaje);
+alert(mensaje);
