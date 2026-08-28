dc26cf9175ee54d04337b72896c4c8a2bb6ed70c
Florencia Adán <florenciaadan@hotmail.com>
2026-08-28 10:18:50 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,33 @@
-// pido datos nombres y dos umeoros (ver lo de lucas )
-
-const nombre = prompt("ingrese su nombre");
-const producto = prompt("que producto queres comprar");
-const precio = parseFloat(prompt("cual es el precio del producto?"));
-const cantidad = parseInt(prompt("que cantidad queres comprar de ese producto??"));
-
-// uso const
-// calculo presupuesto
-const total = precio * cantidad;
+// pido datos del producto
 
 let seguir = true;
 
-
-// si es verdadero se ejecuta: 
 while (seguir) {
 
-    // chequeo si el valor es mayor a $100 k, y devuelve mensaje
+    const nombre = prompt("ingrese su nombre");
+    const producto = prompt("que producto queres comprar");
+    const precio = parseFloat(prompt("cual es el precio del producto?"));
+    const cantidad = parseInt(prompt("que cantidad queres comprar de ese producto?"));
+
+    // calculo el total
+    const total = precio * cantidad;
+
+    // chequeo el presupuesto
     if (total > 100000) {
-        console.log("Tu compra supera los $100.000.");
+        console.log("tu compra supera los $100000.");
     } else {
-        console.log("Tu compra está dentro de tu presupuestto");
+        console.log("tu compra esta dentro de tu presupuesto");
     }
 
-    // bucle despues del chequeo 
-    seguir = false;
+    // pregunto si quiere volver a cargar los datos
+    seguir = confirm("queres cambiar los datos de la compra?");
+    
+    if (!seguir) {
+        const mensaje = "hola " + nombre + ", compraste " + cantidad +
+            " unidades de " + producto +
+            ". el total de tu compra es: $" + total;
+
+        console.log(mensaje);
+        alert(mensaje);
+    }
 }
-
-/// resultado 
-const mensaje = "hola " + nombre + ", compraste " + cantidad + 
-    " unidades de " + producto + 
-    ". El total de tu compra es: $" + total;
-
-console.log(mensaje);
-alert(mensaje);
-
-
